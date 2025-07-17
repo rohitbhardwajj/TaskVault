38dba887621b8e95867a0dffce61d44ac98a6c9f
Rohit Bhardwaj <rohitbhardwaj121001@gmail.com>
2025-07-17 19:13:17 +0530

    DOne

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(),tailwindcss()],
-// })
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -21,6 +10,6 @@ export default defineConfig({
     watch: {
       usePolling: true,
     },
+    allowedHosts: ['taskvault-uyyd.onrender.com'], // ✅ Add this line
   },
 })
-
